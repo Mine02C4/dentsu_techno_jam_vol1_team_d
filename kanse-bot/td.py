0d0ed9a33f7faa86c71f6f9c9b72b1ea188395e4
Yoshihiko-Miyaichi <miyaichi@gmail.com>
2015-03-16 22:07:16 +0000

    first commit

---
+++ kanse-bot/td.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import datetime
+import os
+import pytz
+import tdclient
+import time
+
+apikey = os.getenv("TD_API_KEY")
+
+def get_kansei():
+    n = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+    n = n - datetime.timedelta(minutes=1)
+    t = n.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+    # for test
+    t = '2015-03-10 11:16:00 JST'
+
+    with tdclient.Client(apikey) as client:
+        job = client.query("eeg_datasets",
+              "SELECT AVG(interest) AS interest"
+              "      ,AVG(concentration) AS concentration"
+              "      ,AVG(drowsiness) AS drowsiness"
+              "      ,AVG(stress) AS stress"
+              "      ,AVG(like) AS like"
+              "  FROM kansei"
+              " WHERE TD_TIME_RANGE(time, '" + t + "',"
+              "                     TD_TIME_ADD('" + t + "', '1m'))"
+              )
+        while not job.finished():
+            time.sleep(1)
+        for row in job.result():
+            return row
+
+if __name__ == '__main__':
+    (interest, concentration, drowsiness, stress, like) = get_kansei()
+    print(interest, concentration, drowsiness, stress, like)
